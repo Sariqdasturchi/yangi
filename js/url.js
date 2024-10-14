0d9324caa27e60cabafa8fdb5e4173c0b60c6755
Suhrob <suhrobgithub22@gmail.com>
2024-10-14 16:29:16 +0500

    final project

--- js/url.js
+++ js/url.js
@@ -13,7 +13,6 @@ const createOrUpdateBaseTag = (newHref) => {
   };
   
   // Misol uchun, <base> tegini yangilash
-  createOrUpdateBaseTag('http://127.0.0.1:5501/');
+  createOrUpdateBaseTag('https://www.alphabet-game.uz/');
   // https://alphabet-uz.netlify.app/
-  // http://127.0.0.1:5500/index.html
   // http://127.0.0.1:5500/index.html
